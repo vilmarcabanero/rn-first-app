3601c8c933d2f08957e41669d561a9118d8585a7
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-05-23 17:14:59 +0800

    9. React Native Apps Are Hard Work!

--- App.js
+++ App.js
@@ -1,34 +1,40 @@
 import React, { useState } from 'react'
-import * as RN from 'react-native'
+import { Button, View, Text, StyleSheet } from 'react-native'
 import styled from 'styled-components'
 
 const App = () => {
 	const [outputText, setOutputText] = useState('Hello G! I like you!')
 
 	return (
-		<RN.View style={styles.container}>
-			<Text>{outputText}</Text>
-			<RN.Button
+		<View style={styles.container}>
+			<Text style={styles.text}>{outputText}</Text>
+			<Button
 				title='Can I ask you something?'
 				onPress={() => setOutputText('Will you be my girlfriend?')}
 			/>
-		</RN.View>
+
+			<Button
+				style={styles.button}
+				title='Refresh'
+				onPress={() => setOutputText('Hello G! I like you!')}
+			/>
+		</View>
 	)
 }
 
-const styles = RN.StyleSheet.create({
+const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
+		width: '100%',
+	},
+	text: {
+		color: 'steelblue',
+		marginBottom: 5,
+		fontSize: 16,
 	},
 })
-
-const Text = styled(RN.Text)`
-	color: steelblue;
-	font-size: 16px;
-	margin-bottom: 5px;
-`
 
 export default App
